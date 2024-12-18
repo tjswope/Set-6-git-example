82ee919030ae3e789d07412c935815060978daba
Tom Swope <tjswope@gmail.com>
2024-12-18 12:31:05 -0500

    Initial Commit

---
+++ Set 6 git example/Set_6_git_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_6_git_exampleApp.swift
+//  Set 6 git example
+//
+//  Created by Swope, Thomas on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Set_6_git_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
